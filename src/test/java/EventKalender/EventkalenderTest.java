2ff425018bbc57f7ec597f42f60ea6de6c28e86d
Treilind <52202041@edu.campus02.at>
2023-02-07 08:16:57 +0100

    Convert project into mvn project and implement junit5 dependency

---
+++ src/test/java/EventKalender/EventkalenderTest.java
@@ -0,0 +1,37 @@
+package EventKalender;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventkalenderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void printEvents() {
+    }
+
+    @Test
+    void findbyTitle() {
+    }
+
+    @Test
+    void getByLocation() {
+    }
+
+    @Test
+    void getMostExpensiveByOrt() {
+    }
+
+    @Test
+    void getCountEventsByLocation() {
+    }
+}
